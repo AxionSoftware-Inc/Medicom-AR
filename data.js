95cd0794749489081cf7291f1b208a158755eff8
shaxzodoken <shaxzodturayev123@gmail.com>
2025-12-30 01:26:13 +0500

    worked

---
+++ data.js
@@ -0,0 +1,34 @@
+// data.js - Barcha qavatlar bazasi
+
+const FLOORS_DATABASE = {
+    // 1-QAVAT
+    1: [
+        { id: 1, name: "101 - Qabulxona", floor: 1, side: "left", distance: 12 },
+        { id: 2, name: "102 - Laboratoriya", floor: 1, side: "left", distance: 18 },
+        { id: 3, name: "103 - Rentgen", floor: 1, side: "right", distance: 8 },
+        { id: 4, name: "Oshxona", floor: 1, side: "right", distance: 25 },
+        { id: 5, name: "2-qavatga zinapoya", floor: 1, side: "center", distance: 30 }
+    ],
+
+    // 2-QAVAT
+    2: [
+        { id: 201, name: "201 - Terapevt", floor: 2, side: "left", distance: 10 },
+        { id: 202, name: "202 - Lor", floor: 2, side: "right", distance: 15 },
+        { id: 203, name: "Bosh Shifokor", floor: 2, side: "center", distance: 20 }
+    ],
+
+    // 3-QAVAT
+    3: [
+        { id: 301, name: "301 - Ko'z doktori", floor: 3, side: "left", distance: 12 }
+    ],
+
+    // 4-QAVAT
+    4: [
+        { id: 401, name: "401 - Nevropatolog", floor: 4, side: "right", distance: 12 }
+    ],
+
+    // 5-QAVAT
+    5: [
+        { id: 501, name: "Majlislar zali", floor: 5, side: "left", distance: 30 }
+    ]
+};
